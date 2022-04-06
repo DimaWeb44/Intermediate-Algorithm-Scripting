01c0952916b1f44d9a752dfa05c7eab4005f3335
Dima <88742126+DimaWeb44@users.noreply.github.com>
2022-04-06 21:17:28 +0300

    Create lesson18.js

---
+++ lesson18.js
@@ -0,0 +1,14 @@
+function addTogether() {
+  const [first, second] = arguments;
+  if (typeof(first) !== "number") {
+    return undefined;
+  }
+  else if (second === undefined) {
+    function addSecond(second) {
+      if (typeof(second) !== "number") {
+        return undefined;
+      }
+      else {
+        return first + second;
+      }
+    }
